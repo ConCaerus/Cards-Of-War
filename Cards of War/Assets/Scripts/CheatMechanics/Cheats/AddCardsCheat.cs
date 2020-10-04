d8b81a0cb8ab30633ef81a9811d083fa9e01d603
ConCaerus <connorkeeterheise@trojans.dsu.edu>
2020-10-04 14:42:01 -0500

    code test

---
+++ Cards of War/Assets/Scripts/CheatMechanics/Cheats/AddCardsCheat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddCardsCheat : Cheat {
+    const int cardsToAdd = 2;
+
+    public override float getChargeAmount() {
+        return 2.5f;
+    }
+
+
+    //  this cheat adds card to the user's deck
+    public override void use() {
+        
+    }
+
+
+    public override bool canBeUsed() {
+        return GameObject.FindGameObjectWithTag("Player").GetComponent<Deck>().getNumOfCardsInDeck() > 0 &&
+                GameObject.FindGameObjectWithTag("Opponent").GetComponent<Deck>().getNumOfCardsInDeck() > 0;
+    }
+
+    public override void addToPlayer() {
+        GameObject.FindGameObjectWithTag("Player").AddComponent<AddCardsCheat>();
+    }
+}
